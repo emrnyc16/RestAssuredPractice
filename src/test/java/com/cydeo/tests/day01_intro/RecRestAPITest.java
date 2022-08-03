b95e08a1cc166d417b756ff12c8433a98287d576
emre <emreetosun@gmail.com>
2022-08-02 21:21:53 -0400

    second day

--- src/test/java/com/cydeo/tests/day01_intro/RecRestAPITest.java
+++ src/test/java/com/cydeo/tests/day01_intro/RecRestAPITest.java
@@ -38,4 +38,6 @@ public class RecRestAPITest {
         assertEquals("{}",response.asString());
 
     }
+
+
 }
--- src/test/java/com/cydeo/tests/day02_headers/SpartanHeadersTest.java
+++ src/test/java/com/cydeo/tests/day02_headers/SpartanHeadersTest.java
@@ -1,2 +1,11 @@
-package com.cydeo.tests.day02_headers;public class SpartanHeadersTest {
+package com.cydeo.tests.day02_headers;
+
+public class SpartanHeadersTest {
+
+    /**
+     • When I send a GET request to
+     • spartan_base_url/api/spartans
+     • Then Response STATUS CODE must be 200
+     • And I Should see all Spartans data in JSON format
+     */
 }

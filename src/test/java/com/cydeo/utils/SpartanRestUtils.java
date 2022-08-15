911e99212fe9a02b51b69a56cbd05e34233720d0
emre <emreetosun@gmail.com>
2022-08-15 07:23:18 -0400

    day09

---
+++ src/test/java/com/cydeo/utils/SpartanRestUtils.java
@@ -0,0 +1,35 @@
+package com.cydeo.utils;
+import com.cydeo.pojo.Spartan;
+import com.github.javafaker.Faker;
+
+import static io.restassured.RestAssured.*;
+
+public class SpartanRestUtils {
+
+    private static String baseUrl = ConfigurationReader.getProperty("spartan.api.url");
+
+    public static void deleteSpartanById(int spartanId) {
+
+        //send DELETE request {{baseUrl}}/api/spartans/{id}
+        given().pathParam("id", spartanId)
+                .when().delete(baseUrl + "/spartans/{id}")
+                .then().log().all();
+    }
+
+    public static Spartan getNewSpartan() {
+        //create Faker class object to help us genarate random values
+        Faker random = new Faker();
+        Spartan spartan = new Spartan();
+        spartan.setName(random.name().firstName());
+
+        int num = random.number().numberBetween(1, 3);
+        if (num == 1) {
+            spartan.setGender("Female");
+        } else {
+            spartan.setGender("Male");
+        }
+        spartan.setPhone(random.number().numberBetween(1000000000L, 9999999999L));
+
+        return spartan;
+    }
+}
